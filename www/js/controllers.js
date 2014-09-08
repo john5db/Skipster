6d5be60a5527b6602e287e320f18ca726e303fd9
shakdwipeea <ashakdwipeea@gmail.com>
2014-09-08 20:09:25 +0530

    Difficulty in ngrepeat

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -34,13 +34,8 @@ angular.module('starter.controllers', [])
 })
 
 .controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+  $scope.subjects = [
+    'DLD' , 'EM' , 'DMS' , 'M3' , 'COA' , 'DSC'
   ];
 })
 
