46e8b34a2d8f44714e9663ce94d82e2f280c733a
shakdwipeea <ashakdwipeea@gmail.com>
2014-09-09 17:15:28 +0530

    Fully working for cse

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,13 +7,14 @@ angular.module('starter.controllers', ['AttendanceFactories'])
 
 .controller('PlaylistsCtrl', function($scope,Subjects) {
   $scope.subjects = Subjects.subjects;
-  
+
 })
 
 .controller('UpdateController', function($scope, $stateParams,Subjects) {
   var subjects = Subjects.subjects;
   var curSubject = $stateParams.subjectId;
   var name = "";
+  var action = 0;
 
   for(var i = 0; i < subjects.length; i++) {
     if(':' + subjects[i].id === curSubject) {
@@ -27,15 +28,28 @@ angular.module('starter.controllers', ['AttendanceFactories'])
   $scope.present = function () {
     subjects[i].total++;
     subjects[i].present++;
-    Subjects.update(subjects);
+    Subjects.update(subjects,1);
   }
 
   $scope.absent = function () {
     subjects[i].total++;
-    Subjects.update(subjects);
+    Subjects.update(subjects,0);
   }
 
 
+
+  $scope.undo = function() {
+    var last = Subjects.retrive();
+    if(last == 1) {
+      subjects[i].total--;
+    subjects[i].present--;
+    Subjects.update(subjects,1);
+    }
+    else if(last == 0) {
+      subjects[i].total--;
+    Subjects.update(subjects,0);
+    }
+  }
 
 })
 
--- www/js/factories.js
+++ www/js/factories.js
@@ -3,15 +3,23 @@ angular.module('AttendanceFactories',[])
 	.factory ('Subjects', function () {
 		/*var subjects = */
 
-		var subjects = JSON.parse(window.localStorage['subjects'] || '[{"name":"DLD","id":1,"total":0,"present":0},{"name":"DSC","id":2,"total":0,"present":0},{"name":"M3","id":3,"total":0,"present":0},{"name":"COA","id":4,"total":0,"present":0},{"name":"DMS","id":5,"total":0,"present":0},{"name":"EM","id":6,"total":0,"present":0}]');	
+		var subjects = JSON.parse(window.localStorage['subject'] || '[{"name":"DLD","id":1,"total":0,"present":0},{"name":"DSC","id":2,"total":0,"present":0},{"name":"M3","id":3,"total":0,"present":0},{"name":"COA","id":4,"total":0,"present":0},{"name":"DMS","id":5,"total":0,"present":0},{"name":"EM","id":6,"total":0,"present":0}]');	
 
 
 
-		var update = function(subjects) {
-			window.localStorage['subjects'] = JSON.stringify(subjects);
+		var update = function(subjects,action) {
+			window.localStorage['subject'] = JSON.stringify(subjects);
+			window.localStorage['lastAction'] = JSON.stringify(action);
 		}
+
+		var retrive = function() {
+			return JSON.parse(window.localStorage['lastAction'] || '{}');
+		}
+
+
 		return {
 			subjects: subjects ,
-			update: update
+			update: update ,
+			retrive: retrive
 		}
 	});
